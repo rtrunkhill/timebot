606dc500bad5f388181f4e3458e0b9b664d57464
Rory Trunkhill <rtrunkhill@gmail.com>
2018-03-22 22:43:47 +0000

    two new bugs, last bugs?

--- timebot.rb
+++ timebot.rb
@@ -2,6 +2,8 @@ require 'discordrb'
 puts "The current UTC time is: #{Time.now}"
 # Create a bot
 # Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
+
+
 # done once, afterwards, you can remove this part if you want
 puts "This timebot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
@@ -15,27 +17,37 @@ bot.message(with_text: '!timebot list') do |event|
 end
 
 # need to figure out going backwards over teh date line  eg 1000 +0 -11 currently returns -85am
-bot.message(start_with: '!timebot') do |event|
+bot.message(with_text: '!timebot') do |event|
     t = Time.now
-    event.respond "the current time UTC is: #{t.strftime("%H%M")}"
+    event.respond "The current time UTC is: #{t.strftime("%H%M")}\nEnter your conversion. **_Example:_** _2100 +3 -4_"
+    doLoop(event)
+end
 
-    event.user.await(:conversion) do |convert|
-        # if NoMethodError
-        #     event.respond "You broke it! Try again?  Type help if you need it" 
-        # end
+def doLoop(event)
+       event.user.await(:conversion) do |convert|
         if convert.content == 'list'
-            event.respond "Here is a list of all the timezones: https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations "
+            event.respond "Here is a list of all the timezones: https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations.  Enter your conversion."
         elsif convert.content == 'help'
-            event.respond "Enter time to be converted using the 24hr clock, from which timezone, and to which timezone. Example: 2100, +3, -4"
+            event.respond "Enter time to be converted using the 24hr clock, from which timezone, and to which timezone \nTimezones MUST be a numerical value. \n**_Example:_** _2100 +3 -4_\nlist for a list of all timezones\nexit to close Timebot"
+        elsif convert.content == 'exit'
+            return event.respond "Fine, I'll go convert for someone else"
+            bot.end
+            
         else
-            string = convert.content.split.map { |x| x.to_i }
+            splitting = convert.content.split(' ')
+            if splitting.count != 3
+                event.respond "You done messed up it should look like this: _2100 +3 -4_"
+            else
+                user_input = splitting.map { |x| x.to_i }
+                # puts splitting
+            end
         end
-        
 
         
         #run time_changer using inputed parameters
         def time_changer(time, zone_a, zone_b)
-            zone_a = zone_a * 100t
+            # return zone_a
+            zone_a = zone_a * 100
             zone_b = zone_b * 100
             new_time = time - (zone_a - zone_b)
             if new_time < 0
@@ -53,10 +65,9 @@ bot.message(start_with: '!timebot') do |event|
             end
             return final_answer
         end
-        event.respond time_changer(string[0], string[1], string[2])
+        event.respond time_changer(user_input[0], user_input[1], user_input[2])
 
-    end
-    
+    end 
 end
 
 bot.run
